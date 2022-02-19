502fa3ee49dac3b139e4f56a120afba52ebfdb95
pizzato <pizzato@users.noreply.github.com>
2022-02-19 21:36:10 +1100

    Auth started to fail. Answer in: https://stackoverflow.com/a/62829153

--- fpldata/__init__.py
+++ fpldata/__init__.py
@@ -230,13 +230,29 @@ class FPLData:
         """
         with requests.Session() as session:
 
+            headers = {
+                'authority': 'users.premierleague.com',
+                'cache-control': 'max-age=0',
+                'upgrade-insecure-requests': '1',
+                'origin': 'https://fantasy.premierleague.com',
+                'content-type': 'application/x-www-form-urlencoded',
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'sec-fetch-site': 'same-site',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-user': '?1',
+                'sec-fetch-dest': 'document',
+                'referer': 'https://fantasy.premierleague.com/my-team',
+                'accept-language': 'en-US,en;q=0.9,he;q=0.8',
+            }
+
             payload = {
                 'password': password,
                 'login': email,
                 'redirect_uri': _EP_LOGIN_REDIRECT,
                 'app': 'plfpl-web'
             }
-            session.post(_EP_LOGIN, data=payload)
+            session.post(_EP_LOGIN, data=payload, headers=headers)
 
             data_ = session.get(_EP_MYTEAM.format(manager_id=my_team)).json()
 
